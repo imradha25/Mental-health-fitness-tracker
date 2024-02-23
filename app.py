fbafd4c7660be4c97b3e0a3d6756765309813fa2
imradha25 <108569912+imradha25@users.noreply.github.com>
2024-02-23 16:52:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
+
 import pickle
-from flask import Flask, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect
+import pandas as pd
 import numpy as np
-import streamlit as st
 
-# Load the trained Linear Regression model
+
+# app = Flask(__name__)
+
+# # Load the trained Linear Regression model
+# with open('linearr_rregression_model.pkl', 'rb') as model_file:
+#     lr_model = pickle.load(model_file)
+app = Flask(__name__,static_folder='static',static_url_path='/static')
 model = pickle.load(open("linearr_rregression_model.pkl", "rb"))
-
-# Create a Flask app
-app = Flask(__name__)
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Extracting data from the form
+    #   if request.form:
+    #     # Handle form data
     float_features = [float(x) for x in request.form.values()]
+    # features = ['Schizophrenia', 'Bipolar_disorder', 'Eating_disorder', 'Anxiety', 'drug_usage', 'depression', 'alcohol']
+    # input_data = [request.form[feature] for feature in features]
+    # float_features = [(x) for x in request.form.values()]
+    # Create a DataFrame from the input data
+    # input_df = pd.DataFrame([input_data], columns=features)
 
     # Make predictions using the loaded model
     features1 = [np.array(float_features)]
@@ -22,99 +36,15 @@ def predict():
 
     return jsonify({'message': prediction1}), 200
 
+    #return render_template('index.html', prediction=prediction)
 
-# Title
-st.title("Mental Fitness Prediction")
-st.markdown('<div class="container">', unsafe_allow_html=True)
+@app.route("/streamlit")
+def streamlit():
+    return redirect("http://localhost:8501/")
 
-# Input fields
-schizophrenia = st.text_input("Schizophrenia")
-bipolar_disorder = st.text_input("Bipolar Disorder")
-eating_disorder = st.text_input("Eating Disorder")
-anxiety = st.text_input("Anxiety")
-drug_usage = st.text_input("Drug Usage")
-depression = st.text_input("Depression")
-alcohol = st.text_input("Alcohol")
-
-if st.button("Predict", key="predict-button"):
-    # Prepare data for prediction
-    data = {
-        'schizophrenia': schizophrenia,
-        'bipolar_disorder': bipolar_disorder,
-        'eating_disorder': eating_disorder,
-        'anxiety': anxiety,
-        'drug_usage': drug_usage,
-        'depression': depression,
-        'alcohol': alcohol
-    }
-
-    # Send POST request to Flask API
-    response = predict()
-
-    # Display prediction result
-    if response.status_code == 200:
-        result = response.json()
-        st.markdown(f'<div class="result">{result["message"]}</div>', unsafe_allow_html=True)
-    else:
-        st.error(f"Error: {response.text}")
-
-st.markdown('</div>', unsafe_allow_html=True)
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000, host='0.0.0.0')
-
-
-
-
-
-
-
-# import pickle
-# from flask import Flask, render_template, request, jsonify, redirect
-# import pandas as pd
-# import numpy as np
-
-
-# # app = Flask(__name__)
-
-# # # Load the trained Linear Regression model
-# # with open('linearr_rregression_model.pkl', 'rb') as model_file:
-# #     lr_model = pickle.load(model_file)
-# app = Flask(__name__,static_folder='static',static_url_path='/static')
-# model = pickle.load(open("linearr_rregression_model.pkl", "rb"))
-# # @app.route('/')
-# # def index():
-# #     return render_template('index.html')
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     # Extracting data from the form
-#     #   if request.form:
-#     #     # Handle form data
-#     float_features = [float(x) for x in request.form.values()]
-#     # features = ['Schizophrenia', 'Bipolar_disorder', 'Eating_disorder', 'Anxiety', 'drug_usage', 'depression', 'alcohol']
-#     # input_data = [request.form[feature] for feature in features]
-#     # float_features = [(x) for x in request.form.values()]
-#     # Create a DataFrame from the input data
-#     # input_df = pd.DataFrame([input_data], columns=features)
-
-#     # Make predictions using the loaded model
-#     features1 = [np.array(float_features)]
-#     prediction = model.predict(features1)
-#     python_list = prediction.tolist()
-#     prediction1 = f'The Mental Fitness is {python_list[0]:.2f}'
-
-#     return jsonify({'message': prediction1}), 200
-
-#     #return render_template('index.html', prediction=prediction)
-
-# @app.route("/streamlit")
-# def streamlit():
-#     return redirect("http://localhost:8501/")
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True, host= '0.0.0.0', port=5000)
+    app.run(debug=True, host= '0.0.0.0', port=5000)
 
 
 
